2eab84389bec1f89bba31d713cb28e85ffae55c1
fabioze86 <fabiozemann@gmail.com>
2026-07-01 23:03:25 -0300

    ajuste de build e novos artigos

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,7 @@ import cloudflare from "@astrojs/cloudflare";
 // github.com/withastro/astro/issues/16542).
 export default defineConfig({
   site: "https://www.azpetshop.com.br",
+  trailingSlash: "always",
   integrations: [mdx(), sitemap()],
   adapter: cloudflare(),
 });
